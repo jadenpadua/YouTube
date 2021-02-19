481a8dc098d0612778e73079742ff09026c97e34
Jaden Padua <46204970+jadenpadua@users.noreply.github.com>
2021-02-18 17:40:27 -0800

    Create 9-merge-intervals.py

---
+++ Power25/9-merge-intervals.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        if len(intervals) == 0 or intervals == None:
+            return []
+        
+        intervals = sorted(intervals, key=lambda x: x[0])
+        
+        merged = []
+        merged.append(intervals[0])
+        
+        for i in range(1,len(intervals)):
+            last = merged[-1]
+            if last[1] >= intervals[i][0]:
+                last[1] = max(last[1], intervals[i][1])
+            
+            else:
+                merged.append(intervals[i])
+                
+        return merged
